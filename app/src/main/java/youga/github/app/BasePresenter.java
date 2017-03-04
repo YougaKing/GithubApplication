276f7085ebda4348c623fdf7eb164f442f88260e
ailisex <ailisex@163.com>
2017-03-04 21:45:16 +0800

    Initial commit

---
+++ app/src/main/java/youga/github/app/BasePresenter.java
@@ -0,0 +1,13 @@
+package youga.github.app;
+
+/**
+ * Created by YougaKing on 2017/3/3.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unSubscribe();
+
+}
---
+++ app/src/main/java/youga/github/app/BaseView.java
@@ -0,0 +1,10 @@
+package youga.github.app;
+
+/**
+ * Created by YougaKing on 2017/3/3.
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
